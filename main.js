05b1ae4f4ec1d699a035531d8ebf24d438e4dc6c
Mohamed Sharif <mohame@gmail.com>
2015-08-15 14:01:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+var names = ["mohamed", "joe", "john","ali","zoe","michael"];
+
+function namesList(){
+	var sortedNames = names.sort();
+	console.log(sortedNames)
+};
+
+
+
+function newFunction(newStudents){
+	var newNames = ["mohamed", "joe", "john","ali","zoe","michael"];
+	newNames.push("newStudents");
+
+};
+
+
+
+
+function numInput(){
+	var $(".buttonInput").click() = $(".acceptInput");
+
+
+
+};
